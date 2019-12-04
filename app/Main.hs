8da31ca104c0709d10508a38ce0e7c55e9d04b71
Elliot Davies <elliot.a.davies@gmail.com>
2019-12-04 21:49:23 +0000

    Day 4

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import           Day01   (problem)
+import           Day04   (problem)
 import           Prelude
 import           Problem (run)
 
---
+++ src/Day04.hs
@@ -0,0 +1,46 @@
+module Day04
+  ( problem
+  ) where
+
+import           Data.Char  (digitToInt)
+import           Data.List  (group)
+import qualified Data.Text  as Text
+import           Prelude
+import           Problem
+
+type In = (Int, Int)
+type Out = Int
+
+parser :: Parser In
+parser input =
+  let (x:y:_) = fmap Text.unpack $ Text.lines input
+   in (read x, read y)
+
+part1 :: Solution In Out
+part1 (min, max) = findMatching criteria [min..max]
+  where
+    criteria xs = increasing xs && any ((>= 2) . length) (group xs)
+
+findMatching :: ([Int] -> Bool) -> [Int] -> Int
+findMatching criteria = length . filter criteria . fmap splitNum
+  where
+    splitNum = fmap digitToInt . show
+
+increasing :: [Int] -> Bool
+increasing = go True
+  where
+    go acc [x]        = acc
+    go acc (x:y:rest) = go (acc && x <= y) (y:rest)
+
+part2 :: Solution In Out
+part2 (min, max) = findMatching criteria [min..max]
+  where
+    criteria xs = increasing xs && any ((== 2) . length) (group xs)
+
+problem :: Problem In Out
+problem =
+  Problem "src/day04.txt" parser part1Examples part1 part2Examples part2
+  where
+    part1Examples = []
+
+    part2Examples = []
