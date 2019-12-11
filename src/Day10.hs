a7d9c2e8f081d6f485aff72e3bc0d22074020b57
Elliot Davies <elliot.a.davies@gmail.com>
2019-12-11 15:39:46 +0000

    Day 10 pt 2

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,62 +1,102 @@
 module Day10
   ( problem
-  , visible
-  , parser
   ) where
 
 import           Data.List       (sortOn)
 import qualified Data.Map.Strict as Map
 import qualified Data.Text       as Text
+import           Data.Tuple      (swap)
 import           Linear.V2       (V2 (..))
 import           Prelude
 import           Problem
-import           Utils.Coords    (Coords, Grid, manhattan)
-
+import           Utils.Coords    (Coords, manhattan)
 
 data Position = Empty | Asteroid
   deriving (Eq, Show)
 
-type Angle = V2 Int
+newtype Ratio = Ratio (V2 Int)
+  deriving (Eq, Ord)
 
-type In = Grid Position
-type Out = (Coords, Int)
+type In = [Coords]
+type Out = Int
 
 parser :: Parser In
-parser = foldl parseLine Map.empty . zip [0..] . Text.lines
+parser = Map.keys . Map.filter (== Asteroid) . foldl parseLine Map.empty . zip [0..] . Text.lines
   where
     parseLine grid (y,line) = foldl parsePoint grid $ zip [0..] $ Text.unpack line
       where
         parsePoint grid (x,point) =
           Map.insert (V2 x y) (if point == '#' then Asteroid else Empty) grid
 
-part1 :: Solution In Out
-part1 grid
-  = head
-  . reverse
-  . sortOn snd
-  . Map.assocs
-  $ Map.mapWithKey (\cs _ -> length $ Map.elems $ visible asteroids cs) asteroids
-  where
-    asteroids = Map.filter (== Asteroid) grid
 
-visible :: Grid Position -> Coords -> Map.Map Angle Coords
-visible grid coords
-  = Map.foldrWithKey  (\cs angle -> Map.insertWith nearer angle cs) Map.empty
-  $ Map.mapWithKey    (\k _ -> angleFrom coords k)
-  $ Map.filterWithKey (\k _ -> k /= coords) grid
+part1 :: Solution In Out
+part1 = snd . findStation
+
+
+findStation :: [Coords] -> (Coords, Int)
+findStation asteroids
+  = head $ reverse $ sortOn snd $ fmap (\cs -> (cs, length $ visible cs asteroids)) asteroids
+
+
+visible :: Coords -> [Coords] -> [(Coords, Ratio)]
+visible coords
+  = fmap swap . Map.assocs
+  . foldr (\(cs,ratio) -> Map.insertWith nearer ratio cs) Map.empty -- De-duplicate points along the same line-of-sight
+  . fmap (\cs -> (cs, ratioFrom coords cs))
+  . filter (/= coords)
     where
+      nearer :: Coords -> Coords -> Coords
       nearer cs cs' = if manhattan coords cs < manhattan coords cs' then cs else cs'
 
-angleFrom :: Coords -> Coords -> Angle
-angleFrom v v' = let cs@(V2 x y) = v' - v in (`div` (gcd x y)) <$> cs
+      ratioFrom :: Coords -> Coords -> Ratio
+      ratioFrom v v' = let cs@(V2 x y) = v' - v in Ratio $ (`div` (gcd x y)) <$> cs
+
 
 part2 :: Solution In Out
-part2 input = (V2 0 0, 0)
+part2 asteroids =
+  let V2 x y = (sweep [] (filter (/= station) asteroids)) !! 199
+   in x * 100 + y
+  where
+    station = fst $ findStation asteroids
+
+    sweep :: [Coords] -> [Coords] -> [Coords]
+    sweep destroyed remaining =
+      case remaining of
+        [] -> destroyed
+        cs ->
+          let detected = fmap fst $ reverse $ sortOn snd $ fmap (fmap angle) $ visible station cs
+           in sweep (destroyed ++ detected) $ filter (\k -> not $ k `elem` detected) cs
+
+    angle :: Ratio -> Double
+    angle (Ratio (V2 x y)) = atan2 (fromIntegral x) (fromIntegral y)
 
 problem :: Problem In Out
 problem =
   Problem "src/day10.txt" parser part1Examples part1 part2Examples part2
   where
+    ex = Text.unlines
+      [ ".#..##.###...#######"
+      , "##.############..##."
+      , ".#.######.########.#"
+      , ".###.#######.####.#."
+      , "#####.##.#.##.###.##"
+      , "..#####..#.#########"
+      , "####################"
+      , "#.####....###.#.#.##"
+      , "##.#################"
+      , "#####.##.###..####.."
+      , "..######..##.#######"
+      , "####.##.####...##..#"
+      , ".#####..#.######.###"
+      , "##...#.##########..."
+      , "#.##########.#######"
+      , ".####.#.###.###.#.##"
+      , "....##.##.###..#####"
+      , ".#.#.###########.###"
+      , "#.#.#.#####.####.###"
+      , "###.##.####.##.#..##"
+      ]
+
     part1Examples =
       [ (Text.unlines
         [ ".#..#"
@@ -65,7 +105,7 @@ problem =
         , "....#"
         , "...##"
         ]
-        , (V2 3 4, 8))
+        , 8)
 
       , (Text.unlines
         [ "......#.#."
@@ -79,7 +119,7 @@ problem =
         , "##...#..#."
         , ".#....####"
         ]
-      , (V2 5 8, 33))
+      , 33)
 
       , (Text.unlines
         [ "#.#...#.#."
@@ -93,7 +133,7 @@ problem =
         , "......#..."
         , ".####.###."
         ]
-      , (V2 1 2, 35))
+      , 35)
 
       , (Text.unlines
         [ ".#..#..###"
@@ -107,31 +147,11 @@ problem =
         , ".##...##.#"
         , ".....#.#.."
         ]
-      , (V2 6 3, 41))
+      , 41)
 
-      , (Text.unlines
-        [ ".#..##.###...#######"
-        , "##.############..##."
-        , ".#.######.########.#"
-        , ".###.#######.####.#."
-        , "#####.##.#.##.###.##"
-        , "..#####..#.#########"
-        , "####################"
-        , "#.####....###.#.#.##"
-        , "##.#################"
-        , "#####.##.###..####.."
-        , "..######..##.#######"
-        , "####.##.####...##..#"
-        , ".#####..#.######.###"
-        , "##...#.##########..."
-        , "#.##########.#######"
-        , ".####.#.###.###.#.##"
-        , "....##.##.###..#####"
-        , ".#.#.###########.###"
-        , "#.#.#.#####.####.###"
-        , "###.##.####.##.#..##"
-        ]
-      , (V2 11 13, 210))
+      , (ex, 210)
       ]
 
-    part2Examples = []
+    part2Examples =
+      [ (ex, 802)
+      ]
