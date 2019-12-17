de67afc68ccee9c2a62c6caf1efbf573d8c86561
Elliot Davies <elliot.a.davies@gmail.com>
2019-12-17 01:18:51 +0000

    Naively try to optimise day 16 pt 2

--- src/Day16.hs
+++ src/Day16.hs
@@ -4,6 +4,7 @@ module Day16
 
 import           Data.Char       (digitToInt)
 import           Data.Digits     (digits, unDigits)
+import qualified Data.Map.Strict as Map
 import qualified Data.Text       as Text
 import           Prelude
 import           Problem
@@ -15,20 +16,28 @@ parser :: Parser In
 parser = fmap digitToInt . Text.unpack . Text.strip
 
 part1 :: Solution In Out
-part1 = unDigits 10 . take 8 . head . drop 100 . iterate phase
-
-phase :: [Int] -> [Int]
-phase input = element <$> [1 .. length input]
+part1 input = unDigits 10 . take 8 . head . drop 100 . iterate phase $ input
   where
-    element idx = onesDigit $ sum $ zipWith (*) input (pattern idx)
+    indices = [1 .. length input]
 
-    pattern x = drop 1 $ cycle $ foldMap (replicate x) [0,1,0,-1]
+    pattern x = take (length input) $ drop 1 $ cycle $ foldMap (replicate x) [0,1,0,-1]
 
-    onesDigit 0 = 0
-    onesDigit n = last . digits 10 $ abs n
+    -- Attempt memoisation
+    patterns = foldl (\acc i -> Map.insert i (pattern i) acc) Map.empty indices
+
+    phase xs = calcElement <$> indices
+      where
+        calcElement idx = onesDigit $ sum $ zipWith (*) xs (patterns Map.! idx)
+
+onesDigit :: Int -> Int
+onesDigit 0 = 0
+onesDigit n = last . digits 10 $ abs n
 
 part2 :: Solution In Out
-part2 input = 0
+part2 input =
+  let offset = unDigits 10 $ take 7 input
+      input' = drop offset $ concat $ replicate 10000 input
+   in part1 input'
 
 problem :: Problem In Out
 problem =
@@ -40,4 +49,8 @@ problem =
       , ("69317163492948606335995924319873", 52432133)
       ]
 
-    part2Examples = []
+    part2Examples =
+      [ ("03036732577212944063491565474664", 84462026)
+      , ("02935109699940807407585447034323", 78725270)
+      , ("03081770884921959731165446850517", 53553731)
+      ]
